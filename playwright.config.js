73018b4ffd015c2ca02d3719358e718cd65c3979
Artour <tyxa2017@gmail.com>
2024-01-18 19:34:45 +0200

    commit_for_review

--- playwright.config.js
+++ playwright.config.js
@@ -5,7 +5,7 @@ const { defineConfig, devices } = require("@playwright/test");
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-// require('dotenv').config();
+require("dotenv").config();
 
 /**
  * @see https://playwright.dev/docs/test-configuration
@@ -25,7 +25,10 @@ module.exports = defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
+    httpCredentials: {
+      username: "guest",
+      password: "welcome2qauto",
+    },
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
@@ -34,23 +37,34 @@ module.exports = defineConfig({
   /* Configure projects for major browsers */
   projects: [
     {
+      name: "setup",
+      testMatch: /global\.setup\.js/,
+    },
+    {
       name: "chromium",
-      use: { ...devices["Desktop Chrome"] },
+      use: {
+        ...devices["Desktop Chrome"],
+        storageState: "auth.json",
+        dependencies: ["setup"],
+      },
     },
 
     {
       name: "firefox",
       use: {
-        httpCredentials: {
-          username: "guest",
-          password: "welcome2qauto",
-        },
+        ...devices["Desktop Firefox"],
+        storageState: "auth.json",
+        dependencies: ["setup"],
       },
     },
 
     {
       name: "webkit",
-      use: { ...devices["Desktop Safari"] },
+      use: {
+        ...devices["Desktop Safari"],
+        storageState: "auth.json",
+        dependencies: ["setup"],
+      },
     },
 
     /* Test against mobile viewports. */
