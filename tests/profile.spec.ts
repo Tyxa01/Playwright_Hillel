a925c0d549e48eeaa4fd541389c60cab783e584f
Artour <tyxa2017@gmail.com>
2024-02-03 12:34:37 +0200

    js test converted to ts

--- tests/profile.spec.ts
+++ tests/profile.spec.ts
@@ -2,12 +2,11 @@ import { test, expect } from "@playwright/test";
 import { ProfilePage } from "./POM/profile.page";
 
 test.describe("Profile tests ", () => {
-  test("Profile photo update", async ({ page }) => {
+  test.only("Profile photo update", async ({ page }) => {
     const profilePage = new ProfilePage(page);
-    await page.goto("/panel/profile");
-    await profilePage.profilePageButton.click();
-    await profilePage.editProfile.click();
-    await profilePage.photoLoader;
+    await profilePage.open();
+    await profilePage.editProfileButton.click();
+    await profilePage.setInputFiles("fixtures/logo.png");
     await profilePage.saveButton.click();
     await expect(profilePage.updatePhotoPopup).toBeVisible();
   });
